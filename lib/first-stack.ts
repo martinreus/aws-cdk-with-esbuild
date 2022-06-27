6758ff574a19382afa4f8609ad410d36661fb36e
Martin Reus <martinreus@gmail.com>
2022-06-27 17:13:52 +0100

    First stack

--- lib/first-stack.ts
+++ lib/first-stack.ts
@@ -1,5 +1,6 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { aws_s3 as s3 } from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FirstStack extends Stack {
@@ -8,6 +9,12 @@ export class FirstStack extends Stack {
 
     // The code that defines your stack goes here
 
+    new s3.Bucket(this, "MyFirstBucket", {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      bucketName: "chusme",
+    });
     // example resource
     // const queue = new sqs.Queue(this, 'FirstQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
