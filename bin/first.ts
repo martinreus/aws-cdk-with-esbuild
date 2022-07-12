0a820aba5ee7498fb9c41abcc4b97739a5b772e4
Martin Reus <martinreus@gmail.com>
2022-07-12 10:44:44 +0100

    Initial try at using aws-sdk + esbuild

--- bin/first.ts
+++ bin/first.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { FirstStack } from '../lib/first-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { FirstStack } from "../lib/first-stack";
 
 const app = new cdk.App();
-new FirstStack(app, 'FirstStack', {
+new FirstStack(app, "FirstStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -18,4 +18,8 @@ new FirstStack(app, 'FirstStack', {
   // env: { account: '123456789012', region: 'us-east-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  env: {
+    region: "eu-central-1",
+    account: "960760976236",
+  },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import { Response, S3 } from "aws-sdk";
 import { v4 } from "uuid";
 
-export const test = () => {
-  const random = v4();
-  console.log(`hello world ${random}`);
+export const test = async (event: any, ctx: any) => {
+  const random = "";
+  console.log(event);
+
+  const s3B = new S3();
+
+  console.log(`creating bucket... ${random}`);
+
+  const created = await s3B
+    .createBucket({
+      Bucket: v4(),
+    })
+    .promise();
+
+  console.log("Bucket created: " + JSON.stringify(created));
 };
