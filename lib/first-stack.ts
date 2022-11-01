35459863a4ffb2df7ae0295e5244a72f0ccbd030
Martin Reus <martinreus@gmail.com>
2022-11-01 10:45:05 -0300

    Testing rest api and nested stacks

--- lib/first-stack.ts
+++ lib/first-stack.ts
@@ -1,20 +1,45 @@
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
-import { Construct } from "constructs";
 import * as path from "path";
 
-export class FirstStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+import {
+  App,
+  CfnOutput,
+  NestedStack,
+  NestedStackProps,
+  StackProps,
+  Stack,
+} from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Cors,
+  Deployment,
+  LambdaIntegration,
+  Method,
+  MockIntegration,
+  PassthroughBehavior,
+  RestApi,
+  Stage,
+} from "aws-cdk-lib/aws-apigateway";
 
-    // data store:
-    const testTable = new Table(this, "Companies", {
-      tableName: "testTable",
-      partitionKey: { name: "id", type: AttributeType.STRING },
-      billingMode: BillingMode.PAY_PER_REQUEST,
-      removalPolicy: RemovalPolicy.DESTROY,
+// const fn = new NodejsFunction(this, "chusme", {
+//   entry: path.join(__dirname, "..", "src", "index.ts"),
+//   handler: "test",
+//   runtime: Runtime.NODEJS_16_X,
+// });
+
+export class FirstStack extends Stack {
+  constructor(scope: Construct) {
+    super(scope, "integ-restapi-import-RootStack");
+
+    const restApi = new RestApi(this, "RestApi", {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: ["*"],
+        allowCredentials: true,
+      },
+      deploy: false,
     });
 
     const fn = new NodejsFunction(this, "chusme", {
@@ -23,6 +48,121 @@ export class FirstStack extends Stack {
       runtime: Runtime.NODEJS_16_X,
     });
 
-    testTable.grantReadWriteData(fn);
+    restApi.root.addMethod("POST", new LambdaIntegration(fn));
+
+    const petsStack = new PetsStack(this, {
+      restApiId: restApi.restApiId,
+      rootResourceId: restApi.restApiRootResourceId,
+    });
+    const booksStack = new BooksStack(this, {
+      restApiId: restApi.restApiId,
+      rootResourceId: restApi.restApiRootResourceId,
+    });
+    new DeployStack(this, {
+      restApiId: restApi.restApiId,
+      methods: petsStack.methods.concat(booksStack.methods),
+    });
+
+    new CfnOutput(this, "PetsURL", {
+      value: `https://${restApi.restApiId}.execute-api.${this.region}.amazonaws.com/prod/pets`,
+    });
+
+    new CfnOutput(this, "BooksURL", {
+      value: `https://${restApi.restApiId}.execute-api.${this.region}.amazonaws.com/prod/books`,
+    });
+  }
+}
+
+interface ResourceNestedStackProps extends NestedStackProps {
+  readonly restApiId: string;
+
+  readonly rootResourceId: string;
+}
+
+class PetsStack extends NestedStack {
+  public readonly methods: Method[] = [];
+
+  constructor(scope: Construct, props: ResourceNestedStackProps) {
+    super(scope, "integ-restapi-import-PetsStack", props);
+
+    const api = RestApi.fromRestApiAttributes(this, "RestApi", {
+      restApiId: props.restApiId,
+      rootResourceId: props.rootResourceId,
+    });
+
+    const method = api.root.addResource("pets").addMethod(
+      "GET",
+      new MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+          },
+        ],
+        passthroughBehavior: PassthroughBehavior.NEVER,
+        requestTemplates: {
+          "application/json": '{ "statusCode": 200 }',
+        },
+      }),
+      {
+        methodResponses: [{ statusCode: "200" }],
+      }
+    );
+
+    this.methods.push(method);
+  }
+}
+
+class BooksStack extends NestedStack {
+  public readonly methods: Method[] = [];
+
+  constructor(scope: Construct, props: ResourceNestedStackProps) {
+    super(scope, "integ-restapi-import-BooksStack", props);
+
+    const api = RestApi.fromRestApiAttributes(this, "RestApi", {
+      restApiId: props.restApiId,
+      rootResourceId: props.rootResourceId,
+    });
+
+    const method = api.root.addResource("books").addMethod(
+      "GET",
+      new MockIntegration({
+        integrationResponses: [
+          {
+            statusCode: "200",
+          },
+        ],
+        passthroughBehavior: PassthroughBehavior.NEVER,
+        requestTemplates: {
+          "application/json": '{ "statusCode": 200 }',
+        },
+      }),
+      {
+        methodResponses: [{ statusCode: "200" }],
+      }
+    );
+
+    this.methods.push(method);
+  }
+}
+
+interface DeployStackProps extends NestedStackProps {
+  readonly restApiId: string;
+
+  readonly methods?: Method[];
+}
+
+class DeployStack extends NestedStack {
+  constructor(scope: Construct, props: DeployStackProps) {
+    super(scope, "integ-restapi-import-DeployStack", props);
+
+    const deployment = new Deployment(this, "Deployment", {
+      api: RestApi.fromRestApiId(this, "RestApi", props.restApiId),
+    });
+    if (props.methods) {
+      for (const method of props.methods) {
+        deployment.node.addDependency(method);
+      }
+    }
+    new Stage(this, "Stage", { deployment });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,6 @@ import { v4 } from "uuid";
 const db = new DynamoDB.DocumentClient();
 
 export const test = async (event: any, ctx: any) => {
-  const putRslt = await db
-    .put({
-      Item: {
-        id: v4(),
-        name: `a rand name ${v4()}`,
-      },
-      TableName: "testTable",
-    })
-    .promise();
-
-  console.log("persisted? " + putRslt.$response);
+  console.log("test " + JSON.stringify(event));
+  return {};
 };
