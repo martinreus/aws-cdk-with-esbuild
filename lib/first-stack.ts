0fd2f2c8d6ec2da2fd9765854b2033e9fe186569
Martin Reus <martinreus@gmail.com>
2022-11-06 10:32:15 -0300

    working example

--- lib/first-stack.ts
+++ lib/first-stack.ts
@@ -1,31 +1,19 @@
-import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { Function, Runtime, Tracing } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import * as path from "path";
 
-import {
-  App,
-  CfnOutput,
-  NestedStack,
-  NestedStackProps,
-  StackProps,
-  Stack,
-} from "aws-cdk-lib";
-import { Construct } from "constructs";
+import { NestedStack, NestedStackProps, Stack, StackProps } from "aws-cdk-lib";
 import {
   AccessLogFormat,
   Cors,
-  Deployment,
+  Integration,
   LambdaIntegration,
   LogGroupLogDestination,
-  Method,
   MethodLoggingLevel,
-  MockIntegration,
-  PassthroughBehavior,
   RestApi,
-  Stage,
 } from "aws-cdk-lib/aws-apigateway";
-import { LogGroup, LogRetention, RetentionDays } from "aws-cdk-lib/aws-logs";
-import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { LogGroup, RetentionDays } from "aws-cdk-lib/aws-logs";
+import { Construct } from "constructs";
 
 // const fn = new NodejsFunction(this, "chusme", {
 //   entry: path.join(__dirname, "..", "src", "index.ts"),
@@ -52,148 +40,81 @@ export class FirstStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lg = new LogGroup(this, "rest-api-log-group-main-stack", {
-      logGroupName: "rest-api-log-group-main-stack",
-      retention: RetentionDays.ONE_DAY,
+    const bls = new BusinessLogicStack(this, "aBS");
+
+    const fn = new NodejsFunction(this, "asdasd", {
+      entry: path.join(__dirname, "..", "src", "index.ts"),
+      handler: "test",
+      runtime: Runtime.NODEJS_16_X,
+      tracing: Tracing.ACTIVE,
     });
 
-    const restApi = new RestApi(this, "RestApi", {
-      defaultCorsPreflightOptions,
+    const parent = new LambdaIntegration(fn);
 
-      // deployOptions: {
-      //   stageName: "new",
-      //   accessLogDestination: new LogGroupLogDestination(lg),
-      //   ...deployOpts,
-      // },
-      deploy: false,
-
-      restApiName: "RestApi",
-      description: "Lalalal",
-      minimumCompressionSize: 0,
-      retainDeployments: true,
-      failOnWarnings: true,
+    const ra = new RestApiStack(this, "aRA", {
+      lambda: {
+        integrations: {
+          nestedStak: bls.integration,
+          parentInt: parent,
+        },
+      },
     });
-    // restApi.root.addCorsPreflight({
-    //   allowOrigins: Cors.ALL_ORIGINS,
-    //   allowMethods: Cors.ALL_METHODS,
-    //   allowHeaders: ["*"],
-    //   allowCredentials: true,
-    // });
+    ra.node.addDependency(bls);
+  }
+}
+
+interface ResourceNestedStackProps extends NestedStackProps {}
+
+interface ApiResourceArns {
+  integrations?: { nestedStak: Integration; parentInt: Integration };
+}
+
+class BusinessLogicStack extends NestedStack {
+  public integration: Integration;
+
+  constructor(scope: Construct, id: string, props?: ResourceNestedStackProps) {
+    super(scope, id, props);
 
     const fn = new NodejsFunction(this, "chusme", {
       entry: path.join(__dirname, "..", "src", "index.ts"),
       handler: "test",
       runtime: Runtime.NODEJS_16_X,
+      tracing: Tracing.ACTIVE,
     });
 
-    const inMainMethod = restApi.root
-      .resourceForPath(
-        "inMain"
-        // , {
-        // defaultCorsPreflightOptions,
-        // }
-      )
-      .addMethod("POST", new LambdaIntegration(fn), {
-        apiKeyRequired: false,
-      });
-
-    const petsStack = new PetsStack(this, {
-      restApiId: restApi.restApiId,
-      rootResourceId: restApi.restApiRootResourceId,
-    });
-
-    new DeployStack(this, {
-      restApiId: restApi.restApiId,
-      rootResourceId: restApi.restApiRootResourceId,
-      methods: petsStack.methods.concat(inMainMethod),
-    });
-
-    new CfnOutput(this, "PetsURL", {
-      value: `https://${restApi.restApiId}.execute-api.${this.region}.amazonaws.com/prod/pets`,
-    });
-
-    new CfnOutput(this, "BooksURL", {
-      value: `https://${restApi.restApiId}.execute-api.${this.region}.amazonaws.com/prod/books`,
-    });
+    this.integration = new LambdaIntegration(fn);
   }
 }
 
-interface ResourceNestedStackProps extends NestedStackProps {
-  readonly restApiId: string;
-
-  readonly rootResourceId: string;
+interface RestApiStackProps extends NestedStackProps {
+  lambda: ApiResourceArns;
 }
 
-class PetsStack extends NestedStack {
-  public readonly methods: Method[] = [];
+class RestApiStack extends NestedStack {
+  constructor(scope: Construct, id: string, props: RestApiStackProps) {
+    super(scope, id, props);
 
-  constructor(scope: Construct, props: ResourceNestedStackProps) {
-    super(scope, "integ-restapi-import-PetsStack", props);
-
-    const api = RestApi.fromRestApiAttributes(this, "RestApi", {
-      restApiId: props.restApiId,
-      rootResourceId: props.rootResourceId,
-    });
-
-    const method = api.root
-      // .resourceForPath("webhook")
-      .addResource("pets", { defaultCorsPreflightOptions })
-      .addMethod(
-        "GET",
-        new MockIntegration({
-          integrationResponses: [
-            {
-              statusCode: "200",
-            },
-          ],
-          passthroughBehavior: PassthroughBehavior.NEVER,
-
-          requestTemplates: {
-            "application/json": '{ "statusCode": 200 }',
-          },
-        }),
-        {
-          methodResponses: [{ statusCode: "200" }],
-        }
-      );
-
-    this.methods.push(method);
-  }
-}
-
-interface DeployStackProps extends NestedStackProps {
-  readonly restApiId: string;
-  readonly rootResourceId: string;
-
-  readonly methods?: Method[];
-}
-
-class DeployStack extends NestedStack {
-  constructor(scope: Construct, props: DeployStackProps) {
-    super(scope, "integ-restapi-import-DeployStack", props);
-
-    const api = RestApi.fromRestApiAttributes(this, "RestApi", {
-      restApiId: props.restApiId,
-      rootResourceId: props.rootResourceId,
-    });
-
-    const deployment = new Deployment(this, "Deployment", { api });
-
-    if (props.methods) {
-      for (const method of props.methods) {
-        deployment.node.addDependency(method);
-      }
-    }
-
-    const lg = new LogGroup(scope, "nested-stack-log-group", {
+    const lg = new LogGroup(this, "RestApiStack-log-group", {
       retention: RetentionDays.ONE_DAY,
     });
 
-    new Stage(this, "restApiStage", {
-      deployment,
-      stageName: "with-deploy-stack",
-      accessLogDestination: new LogGroupLogDestination(lg),
-      ...deployOpts,
+    const restApi = new RestApi(this, "RestApi", {
+      defaultCorsPreflightOptions,
+      deployOptions: {
+        stageName: "new",
+        accessLogDestination: new LogGroupLogDestination(lg),
+        ...deployOpts,
+      },
+      retainDeployments: true,
+      failOnWarnings: true,
     });
+
+    restApi.root
+      .resourceForPath("inStack")
+      .addMethod("GET", props.lambda.integrations?.nestedStak!);
+
+    restApi.root
+      .resourceForPath("parent")
+      .addMethod("GET", props.lambda.integrations?.parentInt!);
   }
 }
